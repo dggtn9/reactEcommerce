bb4eaf135e9f26b7050a14a662d0e16bd5d0f757
Daniela <dggtn9@gmail.com>
2024-02-21 16:02:45 -0300

    navbar

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDom from "react-dom";
 import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
-import "bootstrap/dist/css/bootstrap.css"
-var element = <button class= "btn btn-danger">Hello World</button>;
-ReactDom.render(element,document.getElementById("root"));
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App";
+import "./index.css";
+
+
+ReactDom.render(<NavBar/>,document.getElementById("root"));
