8daa5f8ebd3bf1d18b49ea37122287bb5d57b861
Daniela <dggtn9@gmail.com>
2024-02-21 18:14:08 -0300

    clases

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 import React,{ Component } from "react";
-class NavBar extends Component{
-    render(){
-        return (
-        <React.Fragment>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Mi App</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Tienda</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Carrito de compras</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-            </React.Fragment>
-    );}
-};
+import NavBar from "./NavBar";
 
-export default NavBar;
+import MainContent from "./MainContent";
+
+export default class App extends
+Component{
+render()
+{return (
+    <React.Fragment>
+<NavBar/>
+<MainContent/>
+    </React.Fragment>
+)
+}
+}
